e714fcc03e969cc22e6b2c429bef70cafcfe3984
Imad ALILAT <alilat.imad@gmail.com>
2020-09-16 16:40:15 +0200

    Making a better cmd output to make it more understandable

--- src/main/java/partition/App.java
+++ src/main/java/partition/App.java
@@ -10,10 +10,13 @@ public class App {
 	public static void main(String[] args) {
 		List<Integer> theList = Arrays.asList(1, 2, 3 , 4, 5);
 		int thePortionNb = 3;
-				
+			
+	
+		System.out.println("> Input list as param : " + Arrays.toString(theList.toArray()));
+		System.out.println("> Input portion : " + thePortionNb);
 		
 		List<List<Integer>> listPartitions = new Partition(theList, thePortionNb).getSubListsByPortion();
-		System.out.println("Final result is : " + listPartitions);
+		System.out.println("\n => Output result : " + listPartitions);
 		
 	}
 
